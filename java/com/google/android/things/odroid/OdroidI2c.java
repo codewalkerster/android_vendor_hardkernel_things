0927cbc7cc200fedef49d0badd03c57e5194a1a8
Luke go <sangch.go@gmail.com>
2023-01-18 16:03:07 +0900

    ODROID: things: service: i2c: Add more funcs.
    Signed-off-by: Luke go <sangch.go@gmail.com>
    Change-Id: I9077122d65b967c6f62b8537d2a8186db2f1b381

--- java/com/google/android/things/odroid/OdroidI2c.java
+++ java/com/google/android/things/odroid/OdroidI2c.java
@@ -29,9 +29,6 @@ public class OdroidI2c implements Pin {
     private int idx;
     private final String name;
 
-    private static final int BYTE = 1;
-    private static final int WORD = 2;
-
     public OdroidI2c(String name, int i2cNameIdx, int address, int idx) {
         this.idx = idx;
         this.name = name;
@@ -48,44 +45,35 @@ public class OdroidI2c implements Pin {
     }
 
     public byte[] read(int length) {
-        return mI2cNative.readRegBuffer(idx, 0, length);
+        return mI2cNative.read(idx, length);
     }
 
     public byte[] readRegBuffer(int reg, int length) {
         return mI2cNative.readRegBuffer(idx, reg, length);
     }
 
-    public byte readRegByte(int reg) {
-        byte[] buffer = mI2cNative.readRegBuffer(idx, reg, BYTE);
-        byte result = buffer[0];
-        return result;
+    public short readRegWord(int reg) {
+        return mI2cNative.readRegWord(idx, reg);
     }
 
-    public short readRegWord(int reg) {
-        byte[] buffer = mI2cNative.readRegBuffer(idx, reg, WORD);
-        short result = (short)(((buffer[1] & 0xFF)<<8)|(buffer[0] & 0xFF));
-        return result;
+    public byte readRegByte(int reg) {
+        return  mI2cNative.readRegByte(idx, reg);
     }
 
     public boolean write(byte[] buffer, int length) {
-        return mI2cNative.writeRegBuffer(idx, 0, buffer, length);
+        return mI2cNative.write(idx, buffer, length);
     }
 
     public boolean writeRegBuffer(int reg, byte[] buffer, int length) {
         return mI2cNative.writeRegBuffer(idx, reg, buffer, length);
     }
 
-    public boolean writeRegByte(int reg, byte data) {
-        byte[] buffer = new byte[BYTE];
-        buffer[0] = data;
-        return mI2cNative.writeRegBuffer(idx, reg, buffer, BYTE);
+    public boolean writeRegWord(int reg, short data) {
+        return mI2cNative.writeRegWord(idx, reg, data);
     }
 
-    public boolean writeRegWord(int reg, short data) {
-        byte[] buffer = new byte[WORD];
-        buffer[1] = (byte)((data & 0xFF00) >> 8);
-        buffer[0] = (byte)(data & 0xFF);
-        return mI2cNative.writeRegBuffer(idx, reg, buffer, WORD);
+    public boolean writeRegByte(int reg, byte data) {
+        return mI2cNative.writeRegByte(idx, reg, data);
     }
 
     private static class I2cNative {
@@ -97,17 +85,47 @@ public class OdroidI2c implements Pin {
             _close(idx);
         }
 
+        public byte[] read(int idx, int length) {
+            return _read(idx, length);
+        }
+
         public byte[] readRegBuffer(int idx, int reg, int length) {
             return _readRegBuffer(idx, reg, length);
+        }
+
+        public short readRegWord(int idx, int reg) {
+            return _readRegWord(idx, reg);
+        }
+
+        public byte readRegByte(int idx, int reg) {
+            return _readRegByte(idx, reg);
+        }
+
+        public boolean write(int idx, byte[] buffer, int length) {
+            return _write(idx, buffer, length);
         }
 
         public boolean writeRegBuffer(int idx, int reg, byte[] buffer, int length) {
             return _writeRegBuffer(idx, reg, buffer, length);
         }
+
+        public boolean writeRegWord(int idx, int reg, short data) {
+            return _writeRegWord(idx, reg, data);
+        }
+
+        public boolean writeRegByte(int idx, int reg, byte data) {
+            return _writeRegByte(idx, reg, data);
+        }
     }
 
     private static native void _open(int i2cNameIdx, int address, int idx);
     private static native void _close(int idx);
+    private static native byte[] _read(int idx, int length);
     private static native byte[] _readRegBuffer(int idx, int reg, int length);
+    private static native short _readRegWord(int idx, int reg);
+    private static native byte _readRegByte(int idx, int reg);
+    private static native boolean _write(int idx, byte[] buffer, int length);
     private static native boolean _writeRegBuffer(int idx, int reg, byte[] buffer, int length);
+    private static native boolean _writeRegWord(int idx, int reg, short data);
+    private static native boolean _writeRegByte(int idx, int reg, byte data);
 }
